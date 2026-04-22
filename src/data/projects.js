32d9695b1c6748887c7d2bffdd189bbf8e2440b3
gitfromnfsheatxaxa <xadasad67@gmail.com>
2026-04-22 21:27:33 +0500

    update

--- src/data/projects.js
+++ src/data/projects.js
@@ -4,39 +4,35 @@ import Img4 from '../Image/Screenshot 2024-06-06 155751.webp';
 import Img6 from '../Image/img.png';
 import Img7 from '../Image/img_1.png';
 import Img8 from '../Image/img_2.png';
+import Img9 from '../Image/Снимок экрана (284).png';
+import Img10 from '../Image/Снимок экрана (285).png';
 
 export const projects = [
+
   {
-    key: 'waterpump',
+    key: 'Steel Theraphy',
     type: 'EXE',
-    href: 'https://waterpump-page.netlify.app/',
-    img: Img2,
-    title: 'Water Pump Project',
-    desc: 'Commercial site for a water pump business.',
-    tags: ['react', 'business', 'live'],
+    href: 'https://steel-theraphy.vercel.app/',
+    img: Img9,
+    title: 'Gym app',
+    desc: 'Planner',
+    tags: ['Next.js', 'state'],
   },
+
   {
-    key: 'calculator',
+    key: 'Spotify stats',
     type: 'EXE',
-    href: 'https://calculator-by-asad.netlify.app/',
-    img: Img1,
-    title: 'Calculator',
-    desc: 'Classic calculator with clean UI.',
-    tags: ['react', 'ui'],
+    href: 'https://spotify-api-flax.vercel.app/',
+    img: Img10,
+    title: 'Spotify Project',
+    desc: 'Statistic Project',
+    tags: ['Next.js', 'state'],
   },
-  {
-    key: 'gamestore',
-    type: 'EXE',
-    href: 'https://game-store-by-asad.netlify.app/',
-    img: Img4,
-    title: 'Game Store',
-    desc: 'Storefront for browsing and listing games.',
-    tags: ['react', 'store'],
-  },
+
   {
     key: 'MoGlobeTrucking',
     type: 'EXE',
-    href: 'https://moglobetrucking.com',
+    href: 'https://logistics-update.vercel.app/',
     img: Img8,
     title: 'Logistics Project',
     desc: 'US logistics',
@@ -51,6 +47,27 @@ export const projects = [
     desc: 'Interactive game project',
     tags: ['react + Vite', 'state'],
   },
+
+  {
+    key: 'waterpump',
+    type: 'EXE',
+    href: 'https://waterpump-page.netlify.app/',
+    img: Img2,
+    title: 'Water Pump Project',
+    desc: 'Commercial site for a water pump business.',
+    tags: ['react', 'business', 'live'],
+  },
+
+  {
+    key: 'gamestore',
+    type: 'EXE',
+    href: 'https://game-store-by-asad.netlify.app/',
+    img: Img4,
+    title: 'Game Store',
+    desc: 'Storefront for browsing and listing games.',
+    tags: ['react', 'store'],
+  },
+
   {
     key: 'Online School',
     type: 'EXE',
@@ -59,6 +76,15 @@ export const projects = [
     title: 'School Project',
     desc: 'Online School Project',
     tags: ['Next.js', 'state'],
+  },
+  {
+    key: 'calculator',
+    type: 'EXE',
+    href: 'https://calculator-by-asad.netlify.app/',
+    img: Img1,
+    title: 'Calculator',
+    desc: 'Classic calculator with clean UI.',
+    tags: ['react', 'ui'],
   },
 
 
